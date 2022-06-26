ffd2a0c4201cc0653b12595898d1e5e11e8fd67c
vishal tiwari <vishaltiwari13200@gmail.com>
2022-06-27 00:38:50 +0530

    Doubly Linklist in DSA

--- Doubly_Linklist.java
+++ Doubly_Linklist.java
@@ -1,3 +1,7 @@
+
+import java.util.Scanner;
+import java.util.*;
+
 public class Doubly_Linklist{
 
     static class Node
@@ -13,13 +17,35 @@ public class Doubly_Linklist{
             this.prev=null;
         }
     }
-    
-    public void insertfirst()
+    // head Node initialization
+    public Node head;
+    public void insertfirst(int data)
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        Node newNode=new Node(data);
+        System.out.println("press:1 -> To Enter the Data");
+        System.out.println("press:2 -> To Print the Data");
+        n=sc.nextInt();
+        do{
+            if(head==null)
+            {
+                head=newNode;
+                return;
+            }
+        // Main Condition
+            newNode.next=head;
+            head.prev=newNode;
+            head=newNode;
+
+        }while(n==1);
+
+    }
+
+    public void print()
     {
         
     }
-
-
     public static void main(String args[])
     {
 
