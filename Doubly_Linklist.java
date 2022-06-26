4f87fb4fad85db20894bea518097ffda3e19b400
vishal tiwari <vishaltiwari13200@gmail.com>
2022-06-26 23:14:25 +0530

    Doubly Linklist in DSA

---
+++ Doubly_Linklist.java
@@ -0,0 +1,27 @@
+public class Doubly_Linklist{
+
+    static class Node
+    {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data)
+        {
+            this.next=null;
+            this.data=data;
+            this.prev=null;
+        }
+    }
+    
+    public void insertfirst()
+    {
+        
+    }
+
+
+    public static void main(String args[])
+    {
+
+    }
+}
